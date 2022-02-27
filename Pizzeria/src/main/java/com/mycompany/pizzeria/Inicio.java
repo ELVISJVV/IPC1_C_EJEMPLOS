bba873d310fbf5e1662b5f056ededcc9ea13de92
José <josegonzahm@gmail.com>
2022-02-27 01:42:59 -0600

    removiendo espacio

--- Pizzeria/src/main/java/com/mycompany/pizzeria/Inicio.java
+++ Pizzeria/src/main/java/com/mycompany/pizzeria/Inicio.java
@@ -14,8 +14,7 @@ public class Inicio {
          Pizza objeto_pizza1 = new Pizza(ingredientes, "Pan pizza", "Delux", "Grande", 50.5); //Creación de bjeto pizza 1 - Método sobrecargado que recibe un arreglo de Strings como primer parámetro
          Pizza objeto_pizza2 = new Pizza("salsa,queso,piña", "Clásica", "Hawaiana", "Mediana", 55.5); //Creación de bjeto pizza 2 - Método sobrecargado que recibe un String y realiza un Split
          Pizza objeto_pizza3 = new Pizza("salsa,queso,piña", "Clásica", "5 Carnes", "Personal", 30.5); //Creación de bjeto pizza 3  - Método sobrecargado que recibe un String y realiza un Split
-         
-         
+           
          //Utilización de métodos estáticos, no necesito crear una instancia u objeto (Pizzeria objetoPizzeria = new Pizzeria) para utilizarlos.
          Pizzeria.colocarPizza(objeto_pizza1); //Llamada al método estático colocarPizza de la clase Pizzería, se le envía un objeto tipo Pizza (objetoPizza1) como parámetro
          Pizzeria.colocarPizza(objeto_pizza2); //Otra llamada a colocarPizza
